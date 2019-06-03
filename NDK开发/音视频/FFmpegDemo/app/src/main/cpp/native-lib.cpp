51ce86c600b6b1363b51f13abaad8db360e6c409
vergoFeng <1035344215@qq.com>
2019-06-03 19:25:25 +0800

    FFmpeg demo

--- NDK开发/音视频/FFmpegDemo/app/src/main/cpp/native-lib.cpp
+++ NDK开发/音视频/FFmpegDemo/app/src/main/cpp/native-lib.cpp
@@ -3,6 +3,7 @@
 
 extern "C" {
 #include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
 }
 
 extern "C" JNIEXPORT jstring JNICALL
@@ -11,4 +12,61 @@ Java_com_vergo_demo_ffmpeg_MainActivity_stringFromJNI(
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(av_version_info());
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_vergo_demo_ffmpeg_FFmpegPlayer_native_1start(JNIEnv *env, jobject instance, jstring path_,
+                                                      jobject surface) {
+    const char *path = env->GetStringUTFChars(path_, 0);
+    // 调用FFmpeg方法进行视频的绘制
+
+    // 初始化FFmpeg的网络模块
+    avformat_network_init();
+
+    // 总上下文
+    AVFormatContext *avFormatContext = avformat_alloc_context();
+
+    AVDictionary *param = NULL;
+    // 设置超时时间，单位是微秒，1秒=1000000微秒
+    av_dict_set(&param, "timeout", "3000000", 0);
+    // 有个int返回值，0：返回成功；非0：返回失败
+    int ret = avformat_open_input(&avFormatContext, path, NULL, &param);
+    if (ret) {
+        return;
+    }
+
+    // 寻找视频流
+    int video_stream_idx = -1;
+    // 通知FFmpeg将流解析出来
+    avformat_find_stream_info(avFormatContext, NULL);
+    for (int i = 0; i < avFormatContext->nb_streams; ++i) {
+        if(avFormatContext->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+            // 如果这个流的类型是视频流，记录视频流的索引
+            video_stream_idx = i;
+            break;
+        }
+    }
+    // 获取视频流的解码参数
+    // AVCodecParameters里面有对当前视频的宽度，高度，延迟时间等的描述
+    AVCodecParameters *codecpar = avFormatContext->streams[video_stream_idx]->codecpar;
+
+    // 获取解码器
+    AVCodec *avCodec = avcodec_find_decoder(codecpar->codec_id);
+    // 解码器的上下文
+    AVCodecContext *avCodecContext = avcodec_alloc_context3(avCodec);
+    // 将获取视频流的解码参数传入解码器上下文
+    avcodec_parameters_to_context(avCodecContext, codecpar);
+    // 打开解码器
+    avcodec_open2(avCodecContext, avCodec, NULL);
+
+    // 解码，从AVPacket中获取yuv数据
+    // 实例化AVPacket对象
+    AVPacket *avPacket = av_packet_alloc();
+    // 从视频流中读取数据包到AVPacket中
+    // 返回值0：读取成功；<0：读取错误或者到文件末尾
+    av_read_frame(avFormatContext, avPacket);
+//    avcodec_send_packet(avCodecContext, avPacket);
+
+    env->ReleaseStringUTFChars(path_, path);
 }
--- NDK开发/音视频/FFmpegDemo/app/src/main/java/com/vergo/demo/ffmpeg/MainActivity.java
+++ NDK开发/音视频/FFmpegDemo/app/src/main/java/com/vergo/demo/ffmpeg/MainActivity.java
@@ -1,29 +1,63 @@
 package com.vergo.demo.ffmpeg;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+    private SurfaceView mSurfaceView;
+    private FFmpegPlayer fFmpegPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        mSurfaceView = findViewById(R.id.surfaceView);
+
+        fFmpegPlayer = new FFmpegPlayer();
+        fFmpegPlayer.setSurfaceView(mSurfaceView);
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    public void open(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                openLocalVideo();
+            } else {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 123);
+            }
+        }
+    }
+
+    private void openLocalVideo() {
+        File file = new File(Environment.getExternalStorageDirectory()+"/Movies", "input.mp4");
+        fFmpegPlayer.start(file.getAbsolutePath());
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 123) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openLocalVideo();
+            } else {
+                Toast.makeText(this, "权限被禁止，无法获取手机里视频", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
