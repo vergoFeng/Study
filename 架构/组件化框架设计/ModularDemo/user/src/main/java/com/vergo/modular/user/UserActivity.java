848b86ea17b13086f26e6d1acf287b32f1230784
vergoFeng <1035344215@qq.com>
2019-07-24 18:49:54 +0800

    组件化部署

---
+++ 架构/组件化框架设计/ModularDemo/user/src/main/java/com/vergo/modular/user/UserActivity.java
@@ -0,0 +1,16 @@
+package com.vergo.modular.user;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * <p>Created by Fenghj on 2019/7/24.</p>
+ */
+public class UserActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
